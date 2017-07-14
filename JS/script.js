c93d6858028338eba45371d67c1fe06b1d40f382
Jory <jory.seguin@live.ca>
2017-07-13 22:16:39 -0400

    start menus

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
-console.log("script start")
+var animSpeed = 400;
+var numPlayers;
+var p1Symbol;
+var p2Symbol;
+
+function setNumPlayers(num) {
+	numPlayers = num;
+	$("#player-select").slideUp(animSpeed);
+	$("#symbol-select").delay(animSpeed).slideDown(animSpeed);
+};
+
+// ----------------------------------------------------------
+// MAIN FUNCTION
+// ----------------------------------------------------------
+$(document).ready(function(){
+	console.log("script start");
+
+	// Listeners
+	$('#player1-btn').click( function() {
+		setNumPlayers(1);
+	});
+	$('#player2-btn').click( function() {
+		setNumPlayers(2);
+	});
+
+	// Slide down menu
+	$("#player-select").slideDown();
+});

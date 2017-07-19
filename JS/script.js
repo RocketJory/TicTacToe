fa24350e4c5cd5a082f2e5564829fb60a11769d3
Jory <jory.seguin@live.ca>
2017-07-18 21:41:23 -0400

    sticky hover fix?

--- JS/script.js
+++ JS/script.js
@@ -11,6 +11,38 @@ var gameBoard = [['','',''],['','',''],['','','']];
 var rowNum = { bottom:2, middle:1, top:0 };
 var colNum = { left:0, middle:1, right:2 };
 
+/**
+ * Converts :hover CSS to :active CSS on mobile devices.
+ * Otherwise, when tapping a button on a mobile device, the button stays in
+ * the :hover state until the button is pressed.
+ *
+ * Inspired by: https://gist.github.com/rcmachado/7303143
+ * @author  Michael Vartan <michael@mvartan.com>
+ * @version 1.0
+ * @date    2014-12-20
+ */
+function hoverTouchUnstick() {
+// Check if the device supports touch events
+if('ontouchstart' in document.documentElement) {
+// Loop through each stylesheet
+for(var sheetI = document.styleSheets.length - 1; sheetI >= 0; sheetI--) {
+var sheet = document.styleSheets[sheetI];
+// Verify if cssRules exists in sheet
+if(sheet.cssRules) {
+// Loop through each rule in sheet
+for(var ruleI = sheet.cssRules.length - 1; ruleI >= 0; ruleI--) {
+var rule = sheet.cssRules[ruleI];
+// Verify rule has selector text
+if(rule.selectorText) {
+// Replace hover psuedo-class with active psuedo-class
+rule.selectorText = rule.selectorText.replace(":hover", ":active");
+          }
+        }
+      }
+    }
+  }
+}
+
 // Set the number of players, go to next menu
 function setNumPlayers(num) {
 	numPlayers = num;
@@ -186,12 +218,6 @@ function checkForWin(board,sym) {
 // ----------------------------------------------------------
 $(document).ready( function() {
 	console.log("script start");
-
-	// handle hover issues for mobile
-	var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
-	if (!touchsupport){ // browser doesn't support touch
-		document.documentElement.className += " non-touch"
-	}
 
 	// Listeners
 	// Player selects
